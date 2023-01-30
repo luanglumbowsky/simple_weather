8df302779fbe98a393a39506803fa7e8a777c3ab
Luan Glumbowsky <luanwsky1@gmail.com>
2023-01-30 19:34:08 +0100

    Implement Data Source

--- lib/domain/repositories/weather_repository.dart
+++ lib/domain/repositories/weather_repository.dart
@@ -1,9 +1,21 @@
+import 'package:simple_weather/data/remote_data_sources/weather_remote_data_source.dart';
 import 'package:simple_weather/domain/models/weather_model.dart';
 
 class WeatherRepository {
-  Future<WeatherModel?> getWeatherModel({
-    required String city,
-  }) async {
-    return const WeatherModel(city: 'Warsaw', temperature: -5.5);
+  WeatherRepository(this._weatherRemoteDataSource);
+
+  final WeatherRemoteDataSource _weatherRemoteDataSource;
+
+  Future<WeatherModel?> getWeatherModel({required String city}) async {
+    final responseData =
+        await _weatherRemoteDataSource.getWeatherData(city: city);
+
+    if (responseData == null) {
+      return null;
+    }
+    final name = responseData['location']['name'] as String;
+    final temperature = (responseData['current']['temp_c'] + 0.0) as double;
+
+    return WeatherModel(city: name, temperature: temperature);
   }
 }
